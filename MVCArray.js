47efbce2b1cd2945fbcda44930dfb3a23eb223d1
PLDaily <pcd12321@gmail.com>
2017-10-18 21:38:49 +0800

    update

--- MVCArray.js
+++ MVCArray.js
@@ -2,15 +2,14 @@
 
 	var root = this;
 
-  	var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
+  	var ObjProto = Object.prototype;
 
-  	var nativeIsArray = Array.isArray,
+  	var nativeIsArray = Array.isArray;
 
   	/**
   	 * 用于返回当前页面的唯一标识字符串
   	 */
   	var _counter = 1;
-
 
 	/**
 	 * 判断给定的对象是否为数组
@@ -25,33 +24,47 @@
 	/**
 	 * 判断给定的对象是否为数组
 	 * @param  {function}  source 要检测的对象
+	 * 
 	 * @return {Boolean}        如果是函数则为true，不是则为false
 	 */
 	var isFunction = function(source) {
         return '[object Function]' == Object.prototype.toString.call(source);
 	};
 
+	/**
+	 * MVCArray构造函数
+	 * @param {Array} arr 传入一个Array
+	 */
 	var MVCArray = function(arr) {
-		if(isArray(arr)) return false;
+		if(!isArray(arr)) return false;
 		this.arr = arr;
 	}
 
 	/**
-	 * 注册时间监听器
+	 * 注册事件监听器
 	 * @param {string} type    自定义事件名称
 	 * @param {functon} handler 自定义事件被触发时的回调函数
-	 * @param {[type]} key     [description]
-	 * @remark 事件类型区分大小写。如果自定义事件名称不是以小写"on"开头，该方法会给它加上"on"再进行判断，即"click"和"onclick"会被认为是同一种事件。 
 	 */
 	MVCArray.prototype.addEventListener = function(type, handler, key) {
-		if(isFunction(handler)) return;
+		if(!isFunction(handler)) return;
 		if(!this._listeners) this._listeners = {};
-		if(type.indexOf('on') != 0) type = "on" + type;
 
 		if(typeof this._listeners[type] != 'object') this._listeners[type] = {};
 		var id = key || 'TANGRAM__' + (_counter ++).toString(36);
 
 		this._listeners[type][id] = handler;
+	}
+
+	/**
+	 * 注册事件触发器
+	 * @param  {string} type 自定义事件名称
+	 */
+	MVCArray.prototype.trigger = function(type) {
+		if(typeof this._listeners[type] == 'undefined') return;
+		var listener = this._listeners[type];
+		for(var i = 0, l = listener.length; i < l; i++) {
+			listener[i].call(this);
+		}
 	}
 
 	/**
@@ -69,6 +82,7 @@
 	/**
 	 * 获取指定索引处的元素
 	 * @param  {number} num 数组的索引
+	 * 
 	 * @return {string}     返回对应索引的元素
 	 */
 	MVCArray.prototype.getAt = function(num) {
@@ -82,6 +96,7 @@
 	 * 设置数组指定索引处的元素
 	 * @param {number} index   数组的索引
 	 * @param {string} element 数组指定索引处的元素
+	 * @remark 只有一个参数时在数组尾部插入
 	 */
 	MVCArray.prototype.setAt = function(index, element) {
 		if(arguments.length == 1) {
@@ -89,6 +104,7 @@
 		}else {
 			this.arr[arguments[0]] = arguments[1];
 		}
+		this.trigger('insert_at');
 	}
 
 	/**
@@ -97,6 +113,7 @@
 	 */
 	MVCArray.prototype.pop = function() {
 		return this.arr.pop();
+		this.trigger('remove_at');
 	}
 
 	/**
@@ -106,7 +123,29 @@
 	 */
 	MVCArray.prototype.insertAt = function(index, element) {
 		this.arr.splice(index, 1, element);
+		this.trigger('insert_at');
 	}
+
+	/**
+	 * 在指定索引处删除元素
+	 * @param  {number} index 数组的索引
+	 * 
+	 * @return {string}       数组中被删除的元素
+	 */
+	MVCArray.prototype.removeAt = function(index) {
+		return this.arr.splice(index, 1);
+		this.trigger('remove_at');
+	}
+
+	/**
+	 * 获取数组的长度
+	 * @return {number} 返回数组的长度
+	 */
+	MVCArray.prototype.getLength = function() {
+		return this.arr.length;
+	}
+
+	root.MVCArray = MVCArray;
 
 }.call(this));
 
